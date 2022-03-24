2956bcc267a143d671a5ac6015b4dce39e12c770
eli5rose <eli5rose@yahoo.com>
2022-03-24 11:22:39 +0100

    smal changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ValidationButtonComponent } from './validation-button/validation-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValidationButtonComponent
   ],
   imports: [
     BrowserModule
