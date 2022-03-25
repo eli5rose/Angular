094be88c2d8db96861623d4dd6172c7b7f04987c
eli5rose <eli5rose@yahoo.com>
2022-03-25 09:32:01 +0100

    Challenge 3 changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValidationButtonComponent } from './validation-button/validation-button.component';
 import { BlockComponent } from './block/block.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ValidationButtonComponent,
-    BlockComponent
+    BlockComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule
